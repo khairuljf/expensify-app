2a71acdcc4a3d052f14296bbbf3518cabe38e4d0
khairulorange <khairul@orangetoolz.com>
2021-04-23 20:36:40 +0600

    Expensify Initial

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+
+
+
+
+
+
+
+ReactDOM.render(<p>This is my boilder placte </p>, document.getElementById('app'))
