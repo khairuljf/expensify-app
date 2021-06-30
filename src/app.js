dc0f523be96bb57d32c1d57f260ec52b81686599
Khairul Islam <khairul@orangetoolz.com>
2021-06-30 21:50:41 +0600

    React-router-dom install & implement

--- src/app.js
+++ src/app.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './styles/styles.scss';
 
 
 
+const Dashboard = () =>(
+ <div>
+     <p>Loading from Dashboard</p>
+ </div>
+);
+
+const CreateSomething = () =>(
+        <div>
+            <p>Create Something Component</p>
+        </div>
+);
+
+const EditFunction = () =>(
+    <div>
+        <p>Edit component</p>
+    </div>
+)
+
+const route = (
+    
+       <BrowserRouter>
+         <div>
+            <Route path="/" component={Dashboard} ></Route>
+            <Route path="/create" component={CreateSomething} ></Route>
+            <Route path="/edit" component={EditFunction} ></Route>
+         </div>
+       </BrowserRouter>
+    
+)
 
 
 
 
 
-ReactDOM.render(<p>This is my boilder placte </p>, document.getElementById('app'))
+
+
+
+ReactDOM.render(route, document.getElementById('app'))
