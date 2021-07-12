f875e1c670934bc5d41b92ac6802fd4bd183aea6
Khairul Islam <khairul@orangetoolz.com>
2021-07-12 21:41:41 +0600

    Higher order component

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 
 import './styles/styles.scss';
 
