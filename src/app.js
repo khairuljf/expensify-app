6e3d8761b9139e090f3c89cb251d256b16769d50
Khairul Islam <khairul@orangetoolz.com>
2021-11-18 21:57:11 +0600

    style remove from repo

--- src/app.js
+++ src/app.js
@@ -10,21 +10,8 @@ import getVisibleExpense from './selectors/expenses';
 import './styles/styles.scss';
 
 const store = configureStore();
- store.dispatch( addExpense({description:'Gas Bills', ammount:1000}) )
- store.dispatch( addExpense({description:'Water bill', ammount:250}) )
- store.dispatch( addExpense({description:'Electricity Bill', ammount:1500}) )
 
-
-
-const state  = store.getState()
-
-
-//console.log("From App.js", state.expenses)
-
-//const visisbleExpenses =  getVisibleExpense(state.expenses, state.filters)
-
-
-//console.log(visisbleExpenses)
+const state  = store.getState();
 
 const jsx = (
     <Provider store={store}>
