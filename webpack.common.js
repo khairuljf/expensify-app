501f32c55f2248e0e68c81bfdaa3a0c4fa16575c
Khairul Islam <khairul@orangetoolz.com>
2021-11-18 20:18:43 +0600

    webpack upgrade

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: ['./src/app.js'],
+
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /(node_modules)/
+        },
+            {
+                test: /\.s?css$/, // Thats mean s optional // Scss & css will load
+                use: [MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap:true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap:true
+                        }
+                    }
+
+                ]
+            }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+            chunkFilename: "[id].css",
+        }),
+    ],
+};
+
