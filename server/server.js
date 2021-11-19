b3e553bddfb77a2665996903273301f4e5128fad
Khairul Islam <khairul@orangetoolz.com>
2021-11-19 23:28:13 +0600

    service port dynamic for heroku

--- server/server.js
+++ server/server.js
@@ -10,6 +10,6 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log(`Server is up by ${port}`);
 });
